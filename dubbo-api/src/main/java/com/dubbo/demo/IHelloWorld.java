37f86cec839c1ee0dd035ec0f158e059939bbbbb
jiangcaijun <caijun.jiang@dbappsecurity.com.cn>
2017-06-27 18:25:31 +0800

    项目初始化。dubbo简易实现_利用自定义的注册中心。dubbo-api (注册中心：服务注册与发现的注册中心)

---
+++ dubbo-api/src/main/java/com/dubbo/demo/IHelloWorld.java
@@ -0,0 +1,10 @@
+package com.dubbo.demo;
+
+/**
+ * Created by jiangcaijun on 2017/3/10.
+ */
+public interface IHelloWorld {
+    public String sayHello(String name);
+
+    public Object doSomeThing();
+}
